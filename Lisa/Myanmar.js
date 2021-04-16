6e40477f20ab5fd1837fed4b2db75c1ec944eb78
LisaCaruana <75282802+LisaCaruana@users.noreply.github.com>
2021-04-15 18:12:23 -0600

    Folders

---
+++ Lisa/Myanmar.js
@@ -0,0 +1,16 @@
+// This gets inserted into the div with an id of 'map'
+var Myanmar_map = L.map("map", {
+    center: [21.9162, 95.9560],
+    zoom: 6
+  });
+  
+  // Adding a tile layer (the background map image) to our map
+  // We use the addTo method to add objects to our map
+  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+  }).addTo(Myanmar_map);
