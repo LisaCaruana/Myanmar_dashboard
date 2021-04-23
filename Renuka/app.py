3efa8acb9262ba600f824992874a52afaf061a73
Renuka <75003561+rkafle-2020@users.noreply.github.com>
2021-04-22 22:12:14 -0600

    updated database and code

--- Renuka/app.py
+++ Renuka/app.py
@@ -4,9 +4,11 @@
 import pymongo
 import pandas as pd
 from flask import Flask, render_template, jsonify
+from flask_cors import CORS
 
 # Create an instance of our Flask app.
 app = Flask(__name__)
+CORS(app)
 
 # Create connection variable
 conn = 'mongodb://localhost:27017'
@@ -18,18 +20,18 @@ db = client.MyanmarDB
 # Available routes
 @app.route("/")
 def home():
-    return (
-        f"Welcome to the what is happening in Myanmar DataBase!<br/>"
-        f"<br/>"
-        f"Below route returns all records from database<br/>"
-        f"/api/v1.0/Myanmar_Records<br/>"
+    return render_template('index.html')
+        # f"Welcome to the what is happening in Myanmar DataBase!<br/>"
+        # f"<br/>"
+        # f"Below route returns all records from database<br/>"
+        # f"/api/v1.0/Myanmar_Records<br/>"
       
-    )
+    
 
-@app.route("/api/v1.0/Myanmar_Records")
+@app.route("/api/Myanmar_Records")
 def myanmar():
     # Store collection in a list
-    Myanmar_Records = db.Myanmar_Records.find()   
+    Myanmar_Records = list(db.Myanmar_Records.find())
     # Create empty list and fill with collection records
     data=[]
     for item in Myanmar_Records:
@@ -38,14 +40,12 @@ def myanmar():
         myanmar_dict['event_type']=item['event_type']
         myanmar_dict['sub_event_type']=item['sub_event_type']
         myanmar_dict['actor1']=item['actor1']
-        myanmar_dict['assoc_actor_1']=item['assoc_actor_1']
         myanmar_dict['actor2']=item['actor2']
-        myanmar_dict['assoc_actor_2']=item['assoc_actor_2']
         myanmar_dict['location']=item['location']
         myanmar_dict['latitude']=item['latitude']
         myanmar_dict['longitude']=item['longitude']
         myanmar_dict['fatalities']=item['fatalities']
-    data.append(myanmar_dict)
+        data.append(myanmar_dict)
 
     return jsonify(data)
    
--- Renuka/static/js/app.js
+++ Renuka/static/js/app.js
@@ -1,34 +1,107 @@
-function buildPlot() {
+
+   function buildPlot() {
+    /* data route */
+    const url = "http://localhost:5000/api/Myanmar_Records";
+    d3.json(url).then(function(response) {
+  
+      console.log(response);
+  
+      const data = response;
+  
+      // Grab values from the data json object to build the plots
+    var event_type  = data.map(one_event=>one_event['event_type']);
+    var fatalities = data.map(one_event=>one_event['fatalities']);
+    
+    var trace1 = {
+      type: "bar",
+      name: 'Myanmar_Fatalities',
+      x: event_type,
+      y: fatalities,
+      marker: {
+        color: 'rgb(142,124,195)',
+        // ['rgba(222,45,38,0.8)', 'rgba(204,204,204,1)', 'rgba(204,204,204,1)', 'rgba(204,204,204,1)'],
+        width: 1
+      },
+      
+    }
+
+      // Define the plot layout
+  var layout = {
+    title: "Civil Unrest and Fatalities in Myanmar",
+    xaxis: { title: "Events" },
+    yaxis: { title: "Number of Fatalities" }
+  };
+    Plotly.newPlot("plot", [trace1], layout);
+  })
+}
+  
+buildPlot();
+
+function buildChart() {
   /* data route */
-  const url = "/api/pals";
+  const url = "http://localhost:5000/api/Myanmar_Records";
   d3.json(url).then(function(response) {
 
     console.log(response);
 
     const data = response;
 
-    const layout = {
-      scope: "usa",
-      title: "Pet Pals",
-      showlegend: false,
-      height: 600,
-            // width: 980,
-      geo: {
-        scope: "usa",
-        projection: {
-          type: "albers usa"
-        },
-        showland: true,
-        landcolor: "rgb(217, 217, 217)",
-        subunitwidth: 1,
-        countrywidth: 1,
-        subunitcolor: "rgb(255,255,255)",
-        countrycolor: "rgb(255,255,255)"
-      }
-    };
+    // Grab values from the data json object to build the plots
+  var sub_event_type  = data.map(one_event=>one_event['sub_event_type']);
+  var event_date = data.map(one_event=>one_event['event_date']);
+  
+  var trace2 = {
+    type: "bar",
+    name: 'Myanmar_Fatalities',
+    x: event_date,
+    y: sub_event_type,
+    orientation: 'h',
+          // ['rgba(222,45,38,0.8)', 'rgba(204,204,204,1)', 'rgba(204,204,204,1)', 'rgba(204,204,204,1)'],
+      width: 1
+    
+    }
 
-    Plotly.newPlot("plot", data, layout);
-  });
+    // Define the plot layout
+var layout = {
+  title: "what is happening in Myanmar",
+  xaxis: { title: "Date" },
+  yaxis: { title: "Events" }
+};
+  Plotly.newPlot("chart", [trace2], layout);
+})
 }
 
-buildPlot();
+buildChart();
+
+// This function is called when a dropdown menu item is selected
+function init() {
+  // Grab a reference to the dropdown select element
+  var selector = d3.select("#selDataset");
+  
+  // Use the list of sample names to populate the select options
+  const url = "http://localhost:5000/api/Myanmar_Records";
+  d3.json(url).then((data) => {
+    var sampleDates = data.dates;
+    sampleDates.forEach((sample) => {
+      selector
+        .append("option")
+        .text(sample)
+        .property("value", sample);
+    });
+  
+    // Use the first sample from the list to build the initial plots
+    const firstSample = sampleNames[0];
+    buildCharts(firstSample);
+    buildMetadata(firstSample);
+  });
+  }
+  
+  function optionChanged(newSample) {
+  // Fetch new data each time a new sample is selected
+  buildCharts(newSample);
+  buildMetadata(newSample);
+  }
+  
+    
+  // Initialize the dashboard
+  init();
